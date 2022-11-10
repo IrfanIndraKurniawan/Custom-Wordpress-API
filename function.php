f0fff92b28bc48ec02592a17829379524472cbc7
Irfan Indra Kurniawan <50232836+IrfanIndraKurniawan@users.noreply.github.com>
2022-11-10 15:07:33 +0700

    Create function.php

---
+++ function.php
@@ -0,0 +1,46 @@
+<?php
+//----------------------------------------------------------------------
+// Custom API for My FLutter Project
+// Add last line Function.php
+
+add_action('rest_api_init', function () {
+    register_rest_route('wp/v2', 'latest-posts', [
+        'method' => 'GET',
+        'callback' => 'get_latest_posts_by_category',
+    ]);
+
+    register_rest_route('wp/v2', 'post-details', [
+        'method' => 'GET',
+        'callback' => 'get_post_by_id',
+    ]);
+});
+
+function get_latest_posts_by_category($request)
+{
+    $args = ['category' => $request['category_id']];
+    $posts = get_posts($args);
+
+    if (empty($posts)) {
+        return new WP_Error('empty_category', 'Tidak ada artikel pada kategori ini', ['status' => 404]);
+    }
+    $post_list = [];
+    foreach ($posts as $post) {
+        $post_categories = wp_get_post_categories($post->ID);
+        $cats = [];
+        foreach ($post_categories as $c) {
+            $cat = get_category($c);
+            $cats[] = $cat->name;
+        }
+
+        $post_list = [
+            "id" => $post->ID,
+            "post_date" => $post->post_date,
+            "title" => $post->post_title,
+            "category_name" => array_values($cats)[0],
+            //"image_url" => '',
+        ];
+    }
+    $response = new WP_REST_Response($post_list);
+    $response->set_status(200);
+    return $response;
+}
